d5b1bcaaafeca5318a5727185f0eac3d71beeb6a
Luis Galeana <lsgaleana@gmail.com>
2023-05-25 18:53:27 -0400

    Test

---
+++ README.md
@@ -0,0 +1,10 @@
+---
+title: Toolkit
+emoji: 😎
+colorFrom: blue
+colorTo: red
+sdk: gradio
+sdk_version: 3.32.0
+app_file: app.py
+pinned: false
+---
---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+
+demo = None
+
+def component():
+    with gr.Row():
+        gr.Textbox("Hello")
+        gr.Textbox("World")
+
+
+with gr.Blocks() as demo:
+    for c in [component, component]:
+        c()
+    gr.Button("Hey")
+
+print("heyhey")
+with gr.Blocks() as demo:
+    for c in [component, component]:
+        c()
+    gr.Textbox("World")
