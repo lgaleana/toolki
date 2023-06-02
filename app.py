3bea00c6241639457b6186faa12efe9adbc08f54
Luis Galeana <lsgaleana@gmail.com>
2023-06-01 20:12:30 -0400

    NIT

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with gr.Blocks() as demo:
     <br>
     <br>**AI Task**: Ask ChatGPT to do something for you. Eg, summarize a text.
     <br>**Code Task**: You will need code to do certain things that ChatGPT can't do, like access the internet or iterate over 4k+ tokens.
-    <br> With this task, ChatGPT will generate code and then execute. The code must be generated before executing all tasks.
+    <br> With this task, ChatGPT will generate code and then execute it. The code must be generated before executing all tasks.
     <br>
     <br>Output from previous tasks can be referenced in subsequen tasks with {tn}. Max 10 tasks allowed (for now).
     """
